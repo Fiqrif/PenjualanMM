a3f3054facc3433cdc2444695cce5649a9576631
Fiqrif <firmansah1926@gmail.com>
2024-12-07 10:25:27 +0700

    Add kategori and get kategori

---
+++ get_kategori.php
@@ -0,0 +1,19 @@
+<?php
+include 'connection/connection.php';
+
+if (isset($_GET['id'])) {
+    $categoryId = $_GET['id'];
+
+    $sql = "SELECT * FROM tb_categories WHERE id = $categoryId";
+    $result = mysqli_query($db, $sql);
+
+    if ($result) {
+        $data = mysqli_fetch_assoc($result);
+        echo json_encode($data);
+    } else {
+        echo json_encode(['error' => 'Failed to retrieve category data.']);
+    }
+} else {
+    echo json_encode(['error' => 'Category ID not provided.']);
+}
+?>
